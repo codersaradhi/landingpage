baaa484597bccdf99736fea30e59b9fc878d808e
codersaradhi <chaitanyasaradhi1999@gmail.com>
2024-12-05 00:17:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    < >
+    <div className='bg-slate-700 w-full h-full p-3'>
+      <div className='flex items-center justify-center' >
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Welcome to My react Deployment app</h1>
+      <div className="card">
+        <button className='border-2 p-4 ' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
